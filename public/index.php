b011535bfc3ad211e07c1b84c4582afe847d4a3e
Anastasia Smolyakova <smolyakova_av@mail.ru>
2023-11-12 22:03:34 +0300

    fix index.php

--- public/index.php
+++ public/index.php
@@ -10,7 +10,5 @@ $app->addErrorMiddleware(true, true, true);
 $app->get('/', function ($request, $response) {
     $response->getBody()->write('Welcome to Slim!');
     return $response;
-//    $response->write('Welcome to Slim!');
 });
 $app->run();
-
