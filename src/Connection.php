423419c0161c63ac05c717970c6a506bd70e32b4
Anastasia Smolyakova <smolyakova_av@mail.ru>
2024-01-21 21:41:59 +0300

    fix connection

--- src/Connection.php
+++ src/Connection.php
@@ -11,11 +11,11 @@ final class Connection
         }
 
         if (isset($databaseUrl['host'])) {
-            $params['host'] = $databaseUrl['host'] ?? null;
-            $params['port'] = $databaseUrl['port'] ?? 5432;
-            $params['database'] = $databaseUrl['path'] ? ltrim($databaseUrl['path'], '/') : null;
-            $params['user'] = $databaseUrl['user'] ?? null;
-            $params['password'] = $databaseUrl['pass'] ?? null;
+            $params['host'] = $databaseUrl['host'];
+            $params['port'] = isset($databaseUrl['port']) ? $databaseUrl['port'] : 5432;
+            $params['database'] = isset($databaseUrl['path']) ? ltrim($databaseUrl['path'], '/') : null;
+            $params['user'] = isset($databaseUrl['user']) ? $databaseUrl['user'] : null;
+            $params['password'] = isset($databaseUrl['pass']) ? $databaseUrl['pass'] : null;
         } else {
             $params = parse_ini_file('database.ini');
         }
