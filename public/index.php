5c74d43ea9983a8e99c56161432522005ba8ae54
Anastasia Smolyakova <smolyakova_av@mail.ru>
2024-01-28 21:22:46 +0300

    fix templates

--- public/index.php
+++ public/index.php
@@ -8,7 +8,8 @@ use Valitron\Validator;
 use Carbon\Carbon;
 use App\Connection;
 use GuzzleHttp\Client;
-use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
 use GuzzleHttp\Exception\ConnectException;
 use GuzzleHttp\Exception\TransferException;
 use DiDom\Document;
@@ -16,9 +17,12 @@ use DiDom\Document;
 session_start();
 
 $container = new Container();
-$container->set('renderer', function () {
-    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
-});
+
+$app = AppFactory::createFromContainer($container);
+$app->addErrorMiddleware(true, true, true);
+
+$container->set('router', $app->getRouteCollector()->getRouteParser());
+
 $container->set('flash', function () {
     return new \Slim\Flash\Messages();
 });
@@ -26,11 +30,13 @@ $container->set('connection', function () {
     $conn = new App\Connection();
     return $conn->connect();
 });
-
-$app = AppFactory::createFromContainer($container);
-$app->addErrorMiddleware(true, true, true);
-
-$router = $app->getRouteCollector()->getRouteParser();
+$container->set('renderer', function () use ($container) {
+    $templateVariables = [
+        'router' => $container->get('router'),
+        'flash' => $container->get('flash')->getMessages()
+    ];
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates', $templateVariables);
+});
 
 $app->get('/', function ($request, $response) {
     return $this->get('renderer')->render($response, 'home.phtml');
@@ -66,7 +72,6 @@ $app->get('/urls', function ($request, $response) {
 
 $app->get('/urls/{id}', function ($request, $response, $args) {
     $id = $args['id'];
-    $flash = $this->get('flash')->getMessages();
 
     $allIdData = $this->get('connection')
             ->query('SELECT id FROM urls')
@@ -90,55 +95,52 @@ $app->get('/urls/{id}', function ($request, $response, $args) {
         'id' => $urlData['id'],
         'name' => $urlData['name'],
         'created_at' => $urlData['created_at'],
-        'urlChecks' => $urlChecksData,
-        'flash' => $flash
+        'urlChecks' => $urlChecksData
     ];
     return $this->get('renderer')->render($response, 'urls/show.phtml', $params);
 })->setName('url');
 
-$app->post('/urls', function ($request, $response) use ($router) {
+$app->post('/urls', function ($request, $response) {
     $url = $request->getParsedBodyParam('url');
 
-    $errors = [];
-    $v = new Validator(['name' => $url['name']]);
-    $v->rule('required', 'name')->rule('lengthMax', 'name', 255)->rule('url', 'name');
+    $v = new Validator(['url_name' => $url['name']]);
+    $v->rule('required', 'url_name')->message('URL не должен быть пустым');
+    $v->rule('lengthMax', 'url_name', 255)->message('URL превышает 255 символов');
+    $v->rule('url', 'url_name')->message('Некорректный URL');
 
-    if (empty($url['name'])) {
-        $errors['name'] = 'URL не должен быть пустым';
-    } elseif (!$v->validate()) {
-        $errors['name'] = 'Некорректный URL';
+    if (!$v->validate()) {
+        $params = [
+            'url' => $url['name'],
+            'errors' => $v->errors()
+        ];
+        return $this->get('renderer')->render($response->withStatus(422), 'home.phtml', $params);
     }
 
-    if (empty($errors['name'])) {
-        $parsedUrl = parse_url(strtolower($url['name']));
-        $name = "{$parsedUrl['scheme']}://{$parsedUrl['host']}";
+    $parsedUrl = parse_url(strtolower($url['name']));
+    $name = "{$parsedUrl['scheme']}://{$parsedUrl['host']}";
 
-        $urlIdQuery = 'SELECT id FROM urls WHERE name = :name';
-        $urlIdStmt = $this->get('connection')->prepare($urlIdQuery);
-        $urlIdStmt->execute([':name' => $name]);
-        $urlId = $urlIdStmt->fetch();
+    $urlIdQuery = 'SELECT id FROM urls WHERE name = :name';
+    $urlIdStmt = $this->get('connection')->prepare($urlIdQuery);
+    $urlIdStmt->execute([':name' => $name]);
+    $urlId = $urlIdStmt->fetch();
 
-        if (!empty($urlId)) {
-            $this->get('flash')->addMessage('success', 'Страница уже существует');
-            return $response->withRedirect($router->urlFor('url', ['id' => $urlId['id']]));
-        }
-
-        $created_at = Carbon::now();
-
-        $newUrlQuery = 'INSERT INTO urls(name, created_at) VALUES(:name, :created_at)';
-        $newUrlStmt = $this->get('connection')->prepare($newUrlQuery);
-        $newUrlStmt->execute([':name' => $name, ':created_at' => $created_at]);
-
-        $id = $this->get('connection')->lastInsertId();
-        $this->get('flash')->addMessage('success', 'Страница успешно добавлена');
-        return $response->withRedirect($router->urlFor('url', ['id' => $id]));
+    if (!empty($urlId)) {
+        $this->get('flash')->addMessage('success', 'Страница уже существует');
+        return $response->withRedirect($this->get('router')->urlFor('url', ['id' => $urlId['id']]));
     }
 
-    $params = ['errors' => $errors];
-    return $this->get('renderer')->render($response->withStatus(422), 'home.phtml', $params);
+    $created_at = Carbon::now();
+
+    $newUrlQuery = 'INSERT INTO urls(name, created_at) VALUES(:name, :created_at)';
+    $newUrlStmt = $this->get('connection')->prepare($newUrlQuery);
+    $newUrlStmt->execute([':name' => $name, ':created_at' => $created_at]);
+
+    $id = $this->get('connection')->lastInsertId();
+    $this->get('flash')->addMessage('success', 'Страница успешно добавлена');
+    return $response->withRedirect($this->get('router')->urlFor('url', ['id' => $id]));
 });
 
-$app->post('/urls/{url_id}/checks', function ($request, $response, $args) use ($router) {
+$app->post('/urls/{url_id}/checks', function ($request, $response, $args) {
     $url_id = $args['url_id'];
 
     $urlNameQuery = 'SELECT name FROM urls WHERE id = :id';
@@ -149,37 +151,25 @@ $app->post('/urls/{url_id}/checks', function ($request, $response, $args) use ($
     $client = new Client();
 
     try {
-        $res = $client->request('GET', $urlName['name']);
+        $res = $client->request('GET', $urlName['name'], ['allow_redirects' => false]);
         $this->get('flash')->addMessage('success', 'Страница успешно проверена');
-        $status_code = $res->getStatusCode();
+    } catch (ClientException $e) {
+        $res = $e->getResponse();
+        $this->get('flash')->addMessage('warning', 'Проверка была выполнена успешно, но сервер ответил c ошибкой');
+        return $response->withRedirect($this->get('router')->urlFor('url', ['id' => $url_id]));
+    } catch (ServerException $e) {
+        $res = $e->getResponse();
+        $this->get('flash')->addMessage('warning', 'Проверка была выполнена успешно, но сервер ответил c ошибкой');
+        return $response->withRedirect($this->get('router')->urlFor('url', ['id' => $url_id]));
     } catch (ConnectException $e) {
         $this->get('flash')->addMessage('failure', 'Произошла ошибка при проверке, не удалось подключиться');
-        return $response->withRedirect($router->urlFor('url', ['id' => $url_id]));
-    } catch (RequestException $e) {
-        $res = $e->getResponse();
-        $status_code = !is_null($res) ? $res->getStatusCode() : null;
-        $check_created_at = Carbon::now();
-        $newCheckQuery = 'INSERT INTO url_checks(
-                    url_id,
-                    status_code,
-                    created_at
-            ) VALUES(
-                    :url_id,
-                    :status_code,
-                    :check_created_at
-                )';
-        $newCheckStmt = $this->get('connection')->prepare($newCheckQuery);
-        $newCheckStmt->execute([
-            ':url_id' => $url_id,
-            ':status_code' => $status_code,
-            ':check_created_at' => $check_created_at
-        ]);
-        $this->get('flash')->addMessage('warning', 'Проверка была выполнена успешно, но сервер ответил c ошибкой');
-        return $response->withRedirect($router->urlFor('url', ['id' => $url_id]));
+        return $response->withRedirect($this->get('router')->urlFor('url', ['id' => $url_id]));
     } catch (TransferException $e) {
-        $this->get('flash')->addMessage('failure', $e->getMessage());
-        return $response->withRedirect($router->urlFor('url', ['id' => $url_id]));
+        $this->get('flash')->addMessage('failure', 'Упс что-то пошло не так');
+        return $response->withRedirect($this->get('router')->urlFor('url', ['id' => $url_id]));
     }
+
+    $status_code = $res->getStatusCode();
 
     $document = new Document((string) $res->getBody());
     $h1 = $document->first('h1') ? mb_substr(optional($document->first('h1'))->text(), 0, 255) : '';
@@ -215,7 +205,7 @@ $app->post('/urls/{url_id}/checks', function ($request, $response, $args) use ($
         ':check_created_at' => $check_created_at
     ]);
 
-    return $response->withRedirect($router->urlFor('url', ['id' => $url_id]), 302);
+    return $response->withRedirect($this->get('router')->urlFor('url', ['id' => $url_id]));
 });
 
 $app->run();
